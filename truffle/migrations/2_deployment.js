4e53c51b9528ef0f66026eb1c399b9014968b361
Sasha Flores <sashaflores207@gmail.com>
2021-03-18 15:51:02 -0400

    Delete truffle directory

--- truffle/migrations/2_deployment.js
+++
@@ -1,8 +0,0 @@
-const MultiSigWallet = artifacts.require("MultiSigWallet");
-
-module.exports = function (deployer, network, accounts) {
-    const owners = accounts.slice(0, 3)
-    const numConfirmationsRequired = 2
-
-  deployer.deploy(MultiSigWallet, owners, numConfirmationsRequired);
-};
